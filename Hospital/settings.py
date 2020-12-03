8f0c2e7e09527443371a0f271671316b02b3d560
mahajanparth19 <42021696+mahajanparth19@users.noreply.github.com>
2020-12-03 17:29:13 +0530

    Update settings.py

--- Hospital/settings.py
+++ Hospital/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&d*1e%p*@3l&dn_zta@jsualjmia!*_1(=hyrf7a6yw$u5jz_!'
+SECRET_KEY = 'secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
