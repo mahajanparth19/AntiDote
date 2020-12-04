1a1d7b5d03af0f74b3a6e990ba2a7cbe7e2830d2
mahajanparth19 <mahajanparth19@gmail.com>
2020-12-04 18:12:51 +0530

    send report check

--- Users/views.py
+++ Users/views.py
@@ -677,7 +677,7 @@ def activate(request, uidb64, token):
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
-        login(request, user)
+        # login(request, user)
         # return redirect('home')
         return render(request, "Users/confirmation.html", {
             "message": "Thank you for your email confirmation. Now you can login your account."
